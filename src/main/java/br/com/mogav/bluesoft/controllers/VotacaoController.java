0d9740e73fabd81b2206d38217ccd40e55c2d60d
andremg <andre.gavino@duxus.com.br>
2016-07-18 14:40:53 -0300

    Arquivos restantes.

---
+++ src/main/java/br/com/mogav/bluesoft/controllers/VotacaoController.java
@@ -0,0 +1,14 @@
+package br.com.mogav.bluesoft.controllers;
+
+import br.com.caelum.vraptor.Controller;
+
+@Controller
+public class VotacaoController {
+
+    /**
+     * @deprecated CDI eyes only
+     */
+    VotacaoController(){}
+    
+    public void index(){};
+}
