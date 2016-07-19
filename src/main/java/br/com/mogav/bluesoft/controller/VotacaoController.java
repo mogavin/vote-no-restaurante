b5765294af3b491a07bdb6476affdaa0aa5e3731
mogavin <motagavino@gmail.com>
2016-07-19 19:15:50 -0300

    Inclusão de tratamento de erros no método 'votar'.

--- src/main/java/br/com/mogav/bluesoft/controller/VotacaoController.java
+++ src/main/java/br/com/mogav/bluesoft/controller/VotacaoController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.SimpleMessage;
+import br.com.caelum.vraptor.validator.Validator;
 import br.com.mogav.bluesoft.model.Restaurante;
 import br.com.mogav.bluesoft.model.Usuario;
 import br.com.mogav.bluesoft.service.VotacaoService;
@@ -13,17 +15,19 @@ import br.com.mogav.bluesoft.service.VotacaoService;
 public class VotacaoController {
 	
 	private final Result result;
+	private final Validator validator;
 	private final VotacaoService service;
 
     /**
      * @deprecated CDI eyes only
      */
     VotacaoController(){
-    	this(null, null);
+    	this(null, null, null);
     }
     
-    VotacaoController(Result result, VotacaoService service){
+    VotacaoController(Result result, Validator validator, VotacaoService service){
     	this.result = result;
+    	this.validator = validator;
     	this.service = service;
     }
     
@@ -32,7 +36,13 @@ public class VotacaoController {
 
     
 	public void votar(Usuario usuario, Map<Restaurante, Boolean> votosPorRestaurante) {		
-		this.service.registrarVoto(usuario, votosPorRestaurante);
+		try{
+			this.service.registrarVoto(usuario, votosPorRestaurante);
+		}catch(RuntimeException ex){
+			this.validator.add(new SimpleMessage("erro", ex.getMessage()));
+		}
+		
 		this.result.redirectTo(RankingController.class).index();
+		this.validator.onErrorRedirectTo(RankingController.class).index();
 	};
 }
--- src/test/java/br/com/mogav/bluesoft/controller/TesteVotacaoController.java
+++ src/test/java/br/com/mogav/bluesoft/controller/TesteVotacaoController.java
@@ -1,6 +1,10 @@
 package br.com.mogav.bluesoft.controller;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Map;
 
@@ -11,15 +15,23 @@ import com.google.common.collect.ImmutableMap;
 
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.util.test.MockResult;
+import br.com.caelum.vraptor.util.test.MockValidator;
+import br.com.caelum.vraptor.validator.ValidationException;
+import br.com.caelum.vraptor.validator.Validator;
 import br.com.mogav.bluesoft.model.Restaurante;
 import br.com.mogav.bluesoft.model.Usuario;
 import br.com.mogav.bluesoft.service.VotacaoService;
 
 public class TesteVotacaoController {
 
+	private static final Map<Restaurante, Boolean> VOTOS_POR_RESTAURANTE = ImmutableMap.of(
+			Restaurante.GIRAFFAS, true,
+			Restaurante.SUBWAY, false
+	);
 	private static final Usuario USUARIO = new Usuario("Joao", "joao@email.com");
 	
 	private RankingController mockRankingController;
+	private Validator mockValidator;
 	private Result spyResult;
 	private VotacaoService mockService;
 	private VotacaoController controller;
@@ -28,8 +40,9 @@ public class TesteVotacaoController {
 	public void setup(){
 		this.mockRankingController = mock(RankingController.class);
 		this.spyResult = spy(new MockResult());
+		this.mockValidator = new MockValidator();
 		this.mockService = mock(VotacaoService.class);
-		this.controller = new VotacaoController(spyResult, mockService);
+		this.controller = new VotacaoController(spyResult, mockValidator, mockService);
 		//Necessário para testes de redirecionamento para páginas de 'RankingController'
 		doReturn(mockRankingController).when(spyResult).redirectTo(RankingController.class);
 	}
@@ -37,16 +50,21 @@ public class TesteVotacaoController {
 	
 	@Test
 	public void votar(){		
-		Map<Restaurante, Boolean> votosPorRestaurante = ImmutableMap.of(
-				Restaurante.GIRAFFAS, true,
-				Restaurante.SUBWAY, false
-		);
-		
 		//Executamos o método a ser testado
-		this.controller.votar(USUARIO, votosPorRestaurante);		
+		this.controller.votar(USUARIO, VOTOS_POR_RESTAURANTE);		
 		
-		verify(mockService).registrarVoto(USUARIO, votosPorRestaurante);
+		verify(mockService).registrarVoto(USUARIO, VOTOS_POR_RESTAURANTE);
 		//Asseguramos que ocorra redirecionamento para a página de rankings
 		verify(mockRankingController).index();
+	}
+	
+	@Test(expected = ValidationException.class)
+	public void seOcorrerErrosNaVotacaoIncluirNaValidaco(){
+		
+		when(mockService.registrarVoto(USUARIO, VOTOS_POR_RESTAURANTE))
+			.thenThrow(new RuntimeException());
+		
+		//Executamos o método a ser testado
+		this.controller.votar(USUARIO, VOTOS_POR_RESTAURANTE);		
 	}
 }
