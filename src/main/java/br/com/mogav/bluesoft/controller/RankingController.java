a95b74f3309aea43032b7a9fc460c19d2a6f07d7
mogavin <motagavino@gmail.com>
2016-07-20 00:23:42 -0300

    Adequação para o VRaptor

--- src/main/java/br/com/mogav/bluesoft/controller/RankingController.java
+++ src/main/java/br/com/mogav/bluesoft/controller/RankingController.java
@@ -2,21 +2,32 @@ package br.com.mogav.bluesoft.controller;
 
 import java.util.Collection;
 
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
 import br.com.mogav.bluesoft.model.ItemRankingVotos;
 import br.com.mogav.bluesoft.model.Usuario;
 import br.com.mogav.bluesoft.persistencia.VotacaoService;
 
+@Controller
 public class RankingController {
 	
 	private final Result result;
 	private final VotacaoService service;
+	
+	 /**
+     * @deprecated CDI eyes only
+     */
+	RankingController(){
+    	this(null, null);
+    }
 
 	public RankingController(Result result, VotacaoService service) {
 		this.result = result;
 		this.service = service;
 	}
 
+	@Path({"", "/"})
 	public void index(Usuario usuario) {
 		Collection<ItemRankingVotos> rankingUsuario = this.service.listarRankingUsuario(usuario);
 		Collection<ItemRankingVotos> rankingGeral = this.service.listarRankingGeral();
