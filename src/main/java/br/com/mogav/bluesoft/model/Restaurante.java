cb4e46d6b7f3aad3e1f46af31e38fbe6e8654aa9
mogavin <motagavino@gmail.com>
2016-07-19 22:48:50 -0300

    Inclusão de atributo 'descricao'

--- src/main/java/br/com/mogav/bluesoft/model/Restaurante.java
+++ src/main/java/br/com/mogav/bluesoft/model/Restaurante.java
@@ -1,9 +1,19 @@
 package br.com.mogav.bluesoft.model;
 
 public enum Restaurante {
-	MCDONALDS,
-	SUBWAY,
-	WENDYS,
-	OUTBACK,
-	GIRAFFAS
+	MCDONALDS("McDonald's"),
+	SUBWAY("Subway"),
+	WENDYS("Wendy's"),
+	OUTBACK("Outback"),
+	GIRAFFAS("Giraffas");
+	
+	private final String descricao;
+	
+	private Restaurante(String descricao){
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao(){
+		return this.descricao;
+	}
 }
