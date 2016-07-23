41d4396895f4f8f580400294a080c58f6e84bd67
mogavin <motagavino@gmail.com>
2016-07-23 01:09:45 -0300

    Remoção de mocks para testes da view

--- src/main/java/br/com/mogav/bluesoft/controller/RankingController.java
+++ src/main/java/br/com/mogav/bluesoft/controller/RankingController.java
@@ -4,13 +4,10 @@ import java.util.Collection;
 
 import javax.inject.Inject;
 
-import com.google.common.collect.ImmutableList;
-
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
 import br.com.mogav.bluesoft.model.ItemRankingVotos;
-import br.com.mogav.bluesoft.model.Restaurante;
 import br.com.mogav.bluesoft.model.Usuario;
 import br.com.mogav.bluesoft.persistencia.VotacaoService;
 
@@ -37,21 +34,10 @@ public class RankingController {
 	@Path({"", "/"})
 	public void index(Usuario usuario) {
 		//Disponibiliza os rankings para a view
-		Collection<ItemRankingVotos> rankingUsuario = this.service.listarRankingUsuario(usuario);//obterMockItensRanking()
-		Collection<ItemRankingVotos> rankingGeral = this.service.listarRankingGeral();//obterMockItensRanking()
+		Collection<ItemRankingVotos> rankingUsuario = this.service.listarRankingUsuario(usuario);
+		Collection<ItemRankingVotos> rankingGeral = this.service.listarRankingGeral();
 
 		this.result.include("rankingUsuario", rankingUsuario);
 		this.result.include("rankingGeral", rankingGeral);
-	}
-	
-	@Deprecated
-	public static Collection<ItemRankingVotos> obterMockItensRanking(){		
-		return ImmutableList.of(
-			new ItemRankingVotos(Restaurante.OUTBACK, 2, 0),
-			new ItemRankingVotos(Restaurante.GIRAFFAS, 2, 1),
-			new ItemRankingVotos(Restaurante.SUBWAY, 1, 0),
-			new ItemRankingVotos(Restaurante.MCDONALDS, 1, 1),
-			new ItemRankingVotos(Restaurante.WENDYS, 0, 1)
-		);
 	}
 }
